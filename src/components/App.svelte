a92ac556c4acef6521f25ba5de8a1135e49e0850
vivek <viveksdchauhan@gmail.com>
2022-02-15 22:02:13 +0000

    restructure components

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,151 +1,61 @@
 <script>
-	import { ethers } from "ethers";
-	import { rareFetch } from "../static/rare_view_management.js";
+	import Contract from './Contract.svelte';
 	
-	// --- On Reloading or Entering example.com --- 
-	let domain = window.location.hostname;
-	domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
-	console.log(domain)
-	// const alchemy = "https://eth-mainnet.alchemyapi.io/v2/A8RlXYDE-mEf5JidAgy7jSkzLcnVgB3L";
-	// const provider = new ethers.providers.JsonRpcProvider(alchemy);
+	let current_href = window.location.href;
+	let found_domain = false;
+	let collection_name, contract, token_id;
 
-	// get current domain
-	// get collection name
-	function get_collection_name() {
-		let collection;
-		let collection_path = window.location.pathname;
-		collection_path = collection_path.replace('http://', '').replace('https://', '').replace('www.','').replace('opensea.io','').split(/[/?#]/);
-		for (let i = 0; i < collection_path.length; i++) {
-			if (collection_path[i] && collection_path[i] === "collection") {
-				if (collection_path[i+1]) {
-					collection = collection_path[i+1];
-				}
-				break;
-			}
+	function init() {
+		let domain = window.location.hostname;
+		domain = domain.replace('http://', '').replace('https://', '').replace('www.','').split(/[/?#]/)[0];
+		if (domain !== "opensea.io") {
+			found_domain = false;
+			return;
 		}
-		return collection
-	}	
-
-	function extract_contract_details() {
-		// let url = "https://api.opensea.io/api/v1/collection/" + collection;
-		if (provider) {
-			let contract_name = "0xed5af388653567af2f388e6224dc7c4b3241c544";
-			const contract_abi = JSON.parse(`[
-			    {
-		        "inputs": [],
-		        "name": "name",
-		        "outputs": [
-	            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-	            }
-		        ],
-		        "stateMutability": "view",
-		        "type": "function"
-			    },
-			    {
-		        "inputs": [
-	            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-	            }
-		        ],
-		        "name": "tokenURI",
-		        "outputs": [
-	            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-	            }
-		        ],
-		        "stateMutability": "view",
-		        "type": "function"
-			    }
-			]`);
-			let contract = new ethers.Contract(contract_name, contract_abi, provider);
-			console.log(contract)
-			console.log(contract.name());
-			console.log(contract.tokenURI(1))
+		found_domain = true;
+		collection_name = get_collection_from_url();
+		let assets = get_assets_from_url();
+		if (assets) {
+			contract = assets[0];
+			token_id = assets[1];
+		} else {
+			contract = undefined;
+			token_id = undefined;
 		}
 	}
 
-	function load_data() {
-		if (domain === "opensea.io") {
-			// extract_contract_details();
-			// find the current NFT and add our information
-			let articles = document.getElementsByTagName('article');
-			for (let i = 0; i < articles.length; i++) {
-				let article = articles[i];
-
-				if (article.hasChildNodes()) {
-					let contract_address, asset_id;
-					let article_children = article.childNodes;
-					let found_div = false;
-					for (let j = 0; j < article_children.length; j++) {
-						// make sure we're only adding the info once
-						if (article_children[j] && article_children[j].className === 'RareView_AssetInfo--grid') {
-							found_div = true;
-							break;
-						}
-					}
-
-					for (var j = 0; j < article_children.length; j++) {
-						// get the current collection token id
-						let asset_link = article_children[j].getAttribute('href');
-						if (asset_link) {
-							asset_id = asset_link.replace('/assets/', '').split("/")[1]
-							break;
-						}
-					}
-					
-					if (! found_div) {
-						// only inject the content once
-						let div = document.createElement("div"); 
-						div.className = 'RareView_AssetInfo--grid';
-						div.innerHTML = `
-					    <div class="rank-div">
-					    	<span class="rank-data">Rank:</span>
-					    </div>
-							<div class="floor-div">
-					    	<span class="floor-data">Floor:</span>
-					    	<svg height="100%" width="0.5em" version="1.1" viewBox="0 0 653 1063" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" focusable="false" style="fill-rule: evenodd; clip-rule: evenodd; stroke-linejoin: round; stroke-miterlimit: 2;">
-						    	<g transform="matrix(1,0,0,1,-1240,-305)">
-						    		<g transform="matrix(1,0,0,1,435.571,533.916)">
-						    			<g transform="matrix(1,0,0,1,729.844,-228.242)">
-						    				<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
-						    					<path d="M923.983,541.032L1250.06,733.681L1250.06,0L923.983,541.032Z" style="fill: rgb(140, 140, 140); fill-rule: nonzero;"></path>
-						    				</g>
-						    				<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
-						    					<path d="M1250.06,0L1250.06,733.682L1576.02,541.033L1250.06,0Z" style="fill: rgb(52, 52, 52); fill-rule: nonzero;"></path>
-						    				</g>
-						    				<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
-						    					<path d="M923.983,602.813L1250.06,1062.2L1250.06,795.463L923.983,602.813Z" style="fill: rgb(140, 140, 140); fill-rule: nonzero;"></path>
-						    				</g>
-						    				<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
-						    					<path d="M1250.06,795.463L1250.06,1062.2L1576.27,602.813L1250.06,795.463Z" style="fill: rgb(52, 52, 52); fill-rule: nonzero;"></path>
-						    				</g>
-						    				<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
-						    					<path d="M1250.06,392.732L923.983,541.032L1250.06,733.681L1576.02,541.032L1250.06,392.732Z" style="fill-opacity: 0.66; fill-rule: nonzero;"></path>
-						    				</g>
-						    			</g>
-						    		</g>
-						    	</g>
-					    	</svg>
-					    	<span class="floor-price"></span>
-					    </div>
-						`;
-						article.appendChild(div)
-					}
-				}
-			}
-		}
+	function get_collection_from_url() {
+		// get collection name from url
+		let path = window.location.pathname;
+		path = path.replace('http://', '').replace('https://', '').replace('www.','').replace('opensea.io','').replace('/collection', '').split(/[/?#]/)[1];
+		return path;
 	}
 
-	document.addEventListener('scroll', function(e) {
-		load_data();
-	});
+	function get_assets_from_url() {
+		// get contract and token id from url
+		let path = window.location.pathname;
+		if (path.search('\/assets\/') === -1) return;
+		
+		path = path.replace('http://', '').replace('https://', '').replace('www.','').replace('opensea.io','').replace('/assets', '').split(/[/?#]/);
+		path.shift();
+		return path;
+	}
+	
+	init();
+
+	// track url changes
+	setInterval(() => {
+		if (current_href !== window.location.href) {
+			current_href = window.location.href;
+			init();
+		}
+	}, 500);
 </script>
 
-<svelte:window on:hashchange={hashchange}/>
+{#if found_domain}
+	<Contract
+		{contract}
+		{collection_name}
+		{token_id}
+	/>
+{/if}
---
+++ src/components/Contract.svelte
@@ -0,0 +1,144 @@
+<script>
+	import { ethers } from "ethers";
+	import { onMount, afterUpdate } from 'svelte';
+
+	export let contract = undefined;
+	export let collection_name = undefined;
+	export let token_id = undefined;
+
+	function get_contract_data() {
+		// fetch data from solidity contract
+	}
+
+	onMount(() => {
+		if (contract) {
+			get_contract_data();
+		}
+	})
+
+	afterUpdate(() => {
+		if (contract) {
+			get_contract_data();
+		}
+	});
+
+	// const alchemy = "https://eth-mainnet.alchemyapi.io/v2/A8RlXYDE-mEf5JidAgy7jSkzLcnVgB3L";
+	// const provider = new ethers.providers.JsonRpcProvider(alchemy);
+	// const contract_abi = JSON.parse(`[
+	// 		{
+	// 			"inputs": [],
+	// 			"name": "name",
+	// 			"outputs": [
+	// 				{
+	// 					"internalType": "string",
+	// 					"name": "",
+	// 					"type": "string"
+	// 				}
+	// 			],
+	// 			"stateMutability": "view",
+	// 			"type": "function"
+	// 		},
+	// 		{
+	// 			"inputs": [
+	// 				{
+	// 					"internalType": "uint256",
+	// 					"name": "",
+	// 					"type": "uint256"
+	// 				}
+	// 			],
+	// 			"name": "tokenURI",
+	// 			"outputs": [
+	// 				{
+	// 					"internalType": "string",
+	// 					"name": "",
+	// 					"type": "string"
+	// 				}
+	// 			],
+	// 			"stateMutability": "view",
+	// 			"type": "function"
+	// 		}
+	// ]`);
+
+	// if (provider) {
+	// 		let contract = new ethers.Contract(contract_name, contract_abi, provider);
+	// 		console.log(contract)
+	// 		console.log(contract.name());
+	// 		console.log(contract.tokenURI(1))
+	// 	}
+
+	// function load_data() {
+		// extract_contract_details();
+		// find the current NFT and add our information
+		// let articles = document.getElementsByTagName('article');
+		// for (let i = 0; i < articles.length; i++) {
+		// 	let article = articles[i];
+
+		// 	if (article.hasChildNodes()) {
+		// 		let contract_address, asset_id;
+		// 		let article_children = article.childNodes;
+		// 		let found_div = false;
+		// 		for (let j = 0; j < article_children.length; j++) {
+		// 			// make sure we're only adding the info once
+		// 			if (article_children[j] && article_children[j].className === 'RareView_AssetInfo--grid') {
+		// 				found_div = true;
+		// 				break;
+		// 			}
+		// 		}
+
+		// 		for (var j = 0; j < article_children.length; j++) {
+		// 			// get the current collection token id
+		// 			let asset_link = article_children[j].getAttribute('href');
+		// 			if (asset_link) {
+		// 				asset_id = asset_link.replace('/assets/', '').split("/")[1]
+		// 				break;
+		// 			}
+		// 		}
+				
+		// 		if (! found_div) {
+		// 			// only inject the content once
+		// 			let div = document.createElement("div"); 
+		// 			div.className = 'RareView_AssetInfo--grid';
+		// 			div.innerHTML = `
+		// 				<div class="rank-div">
+		// 					<span class="rank-data">Rank:</span>
+		// 				</div>
+		// 				<div class="floor-div">
+		// 					<span class="floor-data">Floor:</span>
+		// 					<svg height="100%" width="0.5em" version="1.1" viewBox="0 0 653 1063" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" focusable="false" style="fill-rule: evenodd; clip-rule: evenodd; stroke-linejoin: round; stroke-miterlimit: 2;">
+		// 						<g transform="matrix(1,0,0,1,-1240,-305)">
+		// 							<g transform="matrix(1,0,0,1,435.571,533.916)">
+		// 								<g transform="matrix(1,0,0,1,729.844,-228.242)">
+		// 									<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
+		// 										<path d="M923.983,541.032L1250.06,733.681L1250.06,0L923.983,541.032Z" style="fill: rgb(140, 140, 140); fill-rule: nonzero;"></path>
+		// 									</g>
+		// 									<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
+		// 										<path d="M1250.06,0L1250.06,733.682L1576.02,541.033L1250.06,0Z" style="fill: rgb(52, 52, 52); fill-rule: nonzero;"></path>
+		// 									</g>
+		// 									<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
+		// 										<path d="M923.983,602.813L1250.06,1062.2L1250.06,795.463L923.983,602.813Z" style="fill: rgb(140, 140, 140); fill-rule: nonzero;"></path>
+		// 									</g>
+		// 									<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
+		// 										<path d="M1250.06,795.463L1250.06,1062.2L1576.27,602.813L1250.06,795.463Z" style="fill: rgb(52, 52, 52); fill-rule: nonzero;"></path>
+		// 									</g>
+		// 									<g transform="matrix(1,0,0,1,-849.263,0.0312411)">
+		// 										<path d="M1250.06,392.732L923.983,541.032L1250.06,733.681L1576.02,541.032L1250.06,392.732Z" style="fill-opacity: 0.66; fill-rule: nonzero;"></path>
+		// 									</g>
+		// 								</g>
+		// 							</g>
+		// 						</g>
+		// 					</svg>
+		// 					<span class="floor-price"></span>
+		// 				</div>
+		// 			`;
+		// 			article.appendChild(div)
+		// 		}
+		// 	}
+		// }
+	// }
+
+	// document.addEventListener('scroll', function(e) {
+	// 	load_data();
+	// });
+	
+	// load_data();
+</script>
